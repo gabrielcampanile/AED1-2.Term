c8b5f8682d454ce247a889ef6b4b4484d8a0b9c6
Gabriel Belchior Vieira Campanile <campanile@unifesp.br>
2023-12-08 15:49:37 -0300

    last class

--- 11.23 - Grafos/grafos.c
+++ 11.23 - Grafos/grafos.c
@@ -4,78 +4,91 @@
 
 #define N 10
 
-typedef struct{
-char nome[30]; //informacao do no
-int index; //indice do No no grafo
-}TipoItem;
+typedef struct
+{
+    char nome[30]; // informacao do no
+    int index;     // indice do No no grafo
+} TipoItem;
 
-typedef struct{
-TipoItem l[N]; //lista sequencial de nos
-int qtd;
-}TipoLista;
+typedef struct
+{
+    TipoItem l[N]; // lista sequencial de nos
+    int qtd;
+} TipoLista;
 
-typedef struct{
-TipoLista lista; //lista linear de nos do grafo
-int adj[N][N]; //matriz de adjacencia N x N
-}TipoGrafoMatrizAdj;
+typedef struct
+{
+    TipoLista lista; // lista linear de nos do grafo
+    int adj[N][N];   // matriz de adjacencia N x N
+} TipoGrafoMatrizAdj;
 
-int inserirNo(TipoLista *lista, char *nome){
-if(lista->qtd==N) return -1;
-strcpy(lista->l[lista->qtd].nome, nome);
-lista->l[lista->qtd].index = lista->qtd;
-lista->qtd++;
-return 1;
+int inserirNo(TipoLista *lista, char *nome)
+{
+    if (lista->qtd == N)
+        return -1;
+    strcpy(lista->l[lista->qtd].nome, nome);
+    lista->l[lista->qtd].index = lista->qtd;
+    lista->qtd++;
+    return 1;
 }
 
-int buscaNo(TipoLista *lista, char *nome){
-int i;
-for(i=0; i<lista->qtd; i++){
-if(strcmp(lista->l[i].nome, nome)==0)
-return lista->l[i].index;
-}
-return -1;
-}
-
-int criarAresta(TipoGrafoMatrizAdj *grafo, char *no1, char *no2){
-int i1 = buscaNo(&grafo->lista,no1);
-int i2 = buscaNo(&grafo->lista,no2);
-if(i1<0 || i2<0) return -1;
-
-grafo->adj[i1][i2] = 1;
-grafo->adj[i2][i1] = 1; //grafo nao direcionado
-
-return 1;
+int buscaNo(TipoLista *lista, char *nome)
+{
+    int i;
+    for (i = 0; i < lista->qtd; i++)
+    {
+        if (strcmp(lista->l[i].nome, nome) == 0)
+            return lista->l[i].index;
+    }
+    return -1;
 }
 
-void inicializaGrafo(TipoGrafoMatrizAdj *grafo){
-int i,j;
-grafo->lista.qtd=0;
-for(i=0; i<N; i++)
-for(j=0; j<N; j++)
-grafo->adj[i][j]=0;
+int criarAresta(TipoGrafoMatrizAdj *grafo, char *no1, char *no2)
+{
+    int i1 = buscaNo(&grafo->lista, no1);
+    int i2 = buscaNo(&grafo->lista, no2);
+    if (i1 < 0 || i2 < 0)
+        return -1;
+
+    grafo->adj[i1][i2] = 1;
+    grafo->adj[i2][i1] = 1; // grafo nao direcionado
+
+    return 1;
+}
+
+void inicializaGrafo(TipoGrafoMatrizAdj *grafo)
+{
+    int i, j;
+    grafo->lista.qtd = 0;
+    for (i = 0; i < N; i++)
+        for (j = 0; j < N; j++)
+            grafo->adj[i][j] = 0;
 }
 
 int main()
 {
-int i,j;
-TipoGrafoMatrizAdj grafo;
-inicializaGrafo(&grafo);
-inserirNo(&grafo.lista,"Joao");
-inserirNo(&grafo.lista,"Maria");
-inserirNo(&grafo.lista,"Ana");
-inserirNo(&grafo.lista,"Luis");
-inserirNo(&grafo.lista,"Paula");
-inserirNo(&grafo.lista,"Roberto");
-criarAresta(&grafo,"Joao","Maria"); //Joao e Maria sao amigos
-criarAresta(&grafo,"Maria","Ana");
-criarAresta(&grafo,"Paula","Luis");
-//imprime lista de relacoes do grafo
-for(i=0; i<grafo.lista.qtd; i++){
-printf("%s: ",grafo.lista.l[i].nome);
-for(j=0; j<grafo.lista.qtd; j++){
-if(grafo.adj[i][j]) printf(" %s",grafo.lista.l[j].nome);
-}
-printf("¥n");
-}
-return 0;
+    int i, j;
+    TipoGrafoMatrizAdj grafo;
+    inicializaGrafo(&grafo);
+    inserirNo(&grafo.lista, "Joao");
+    inserirNo(&grafo.lista, "Maria");
+    inserirNo(&grafo.lista, "Ana");
+    inserirNo(&grafo.lista, "Luis");
+    inserirNo(&grafo.lista, "Paula");
+    inserirNo(&grafo.lista, "Roberto");
+    criarAresta(&grafo, "Joao", "Maria"); // Joao e Maria sao amigos
+    criarAresta(&grafo, "Maria", "Ana");
+    criarAresta(&grafo, "Paula", "Luis");
+    // imprime lista de relacoes do grafo
+    for (i = 0; i < grafo.lista.qtd; i++)
+    {
+        printf("%s: ", grafo.lista.l[i].nome);
+        for (j = 0; j < grafo.lista.qtd; j++)
+        {
+            if (grafo.adj[i][j])
+                printf(" %s", grafo.lista.l[j].nome);
+        }
+        printf("¥n");
+    }
+    return 0;
 }
--- Atividades de Programação/AP-11.c
+++ Atividades de Programação/AP-11.c
@@ -1,18 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-typedef struct TipoNo{
-int v; //Núm. do vért. destino da aresta
-struct TipoNo *prox; //Ponteiro para próx vért. adj.
-// ... outros atributos de vértices
-}TipoNo;
+typedef struct Node
+{
+    int dado;
+    struct Node *prox;
+} Node;
 
-typedef struct graph{
-int V,E; //V=Número de vértices, E=Número de arestas
-TipoNo **adj;
-}TipoGrafo;
+typedef struct ListaAdj
+{
+    Node *cabeca;
+} ListaAdj;
 
-int main() {
+typedef struct Grafo
+{
+    int vertices;
+    struct ListaAdj *lista;
+} Grafo;
+
+Grafo *criarGrafo(int N)
+{
+    Grafo *grafo = (Grafo *)malloc(sizeof(Grafo));
+    grafo->vertices = N;
+    grafo->lista = (ListaAdj *)malloc(N * sizeof(ListaAdj));
     
+    for (int i = 0; i < N; i++)
+    {
+        grafo->lista[i].cabeca = NULL;
+    }
+    
+    return grafo;
+}
+
+void novoNoListaAdj(Grafo *grafo, int origem, int destino)
+{
+    Node *novoNo = (Node *)malloc(sizeof(Node));
+    novoNo->dado = destino;
+    novoNo->prox = NULL;
+
+    // If the list is empty or the new node should be inserted before the head
+    if (grafo->lista[origem].cabeca == NULL || grafo->lista[origem].cabeca->dado > destino)
+    {
+        novoNo->prox = grafo->lista[origem].cabeca;
+        grafo->lista[origem].cabeca = novoNo;
+    }
+    else
+    {
+        // Find the node before the point of insertion
+        Node *current = grafo->lista[origem].cabeca;
+        while (current->prox != NULL && current->prox->dado < destino)
+        {
+            current = current->prox;
+        }
+
+        // Insert the new node
+        novoNo->prox = current->prox;
+        current->prox = novoNo;
+    }
+}
+
+void adicionarAresta(Grafo *grafo, int origem, int destino)
+{
+    novoNoListaAdj(grafo, origem, destino);
+}
+
+void imprimirGrafo(Grafo *grafo, int V)
+{
+    Node *temp = grafo->lista[V].cabeca;
+    while (temp != NULL)
+    {
+        printf("%d ", temp->dado);
+        temp = temp->prox;
+    }
+}
+
+int main()
+{
+    int V;
+    scanf("%d", &V);
+
+    int N, M, origem, destino;
+    scanf("%d %d", &N, &M);
+
+    Grafo *grafo = criarGrafo(N);
+
+    for (int i = 0; i < M; i++)
+    {
+        scanf("%d %d", &origem, &destino);
+        adicionarAresta(grafo, origem, destino);
+    }
+
+    imprimirGrafo(grafo, V);
+    return 0;
 }
